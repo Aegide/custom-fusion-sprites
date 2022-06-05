9b9734b69de2999226d0002197deaa1a50eeca64
Aegide <geoffroygressier1@yahoo.fr>
2022-06-05 14:21:32 +0200

    Update sprite downloader

--- scripts/sprite_downloader.py
+++ scripts/sprite_downloader.py
@@ -1,41 +1,45 @@
 # https://github.com/nsadawi/Download-Large-File-From-Google-Drive-Using-Python
+# https://stackoverflow.com/a/54577565
 
 
 import requests
 import os
+from zipfile import ZipFile
 
 
 def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
+    URL = f"https://docs.google.com/uc?confirm=XWPY&export=download&id={id}"
     session = requests.Session()
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+    response = session.get(URL, stream = True)
     save_response_content(response, destination)    
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
 
 
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
     with open(destination, "wb") as f:
         for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:
                 f.write(chunk)
+
+
+def unzip_file(source, destination):
+    try:
+        with ZipFile(source, 'r') as archive:
+            archive.extractall(destination)
+    except:
+        print("\n", "NOT AN ARCHIVE", "\n")
+        os.remove(source)
 
 
 if __name__ == "__main__":
 
 
-    file_id = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH'
-    destination = os.path.join(os.getcwd(), "tmp", "archive.zip")
+    file_id = None
+    archive_path = os.path.join(os.getcwd(), "tmp", "archive.zip")
+    folder_path = os.path.join(os.getcwd(), "tmp")
 
-
-    download_file_from_google_drive(file_id, destination)
+    print(">> START")
+    download_file_from_google_drive(file_id, archive_path)
+    print(">> DOWNLOADED")
+    unzip_file(archive_path, folder_path)
+    print(">> END")
